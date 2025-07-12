d96d63710e14586c899ffcbf98f5e3b3e856eee2
Pritchad25 <pricts29@gmail.com>
2025-07-12 14:34:33 +0000

    Trigger Email Task

--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from listings.tasks import send_booking_confirmation_email
 
+# After booking is created
+send_booking_confirmation_email.delay(booking.customer.email, booking.id)
 # Create your views here.
